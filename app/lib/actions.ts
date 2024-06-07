98097bcb84d8244addb2c8472ea322b01b7d8cce
Emmanuel Eze <44417422+ushnuel@users.noreply.github.com>
2024-06-07 17:27:55 +0100

    authentication using nextauth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { z } from 'zod';
+import { signIn, signOut } from '@/auth';
+import { AuthError } from 'next-auth';
 import { sql } from '@vercel/postgres';
 import { redirect } from 'next/navigation';
 import { revalidatePath } from 'next/cache';
@@ -100,3 +102,27 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
+export const logOut = async () => {
+  await signOut();
+  redirect('/login');
+};
